d6140e0fb1a200b72966a79fa9d0ee6031b8cf8b
Ahmet Demir <43875085+SoftwareEngAhmetDemir@users.noreply.github.com>
2022-06-14 15:40:21 +0300

    Update btn.js

--- btn.js
+++ btn.js
@@ -1,7 +1,7 @@
 
 let footercerez = document.querySelector('.cerez-footer');
 
-document.querySelector('.footer-btn').on('click', function() {
+document.querySelector('.footer-btn').onclick = function() {
     'use strict';
     localStorage.setItem('okundu', true);
     footercerez.addClass('d-none');
