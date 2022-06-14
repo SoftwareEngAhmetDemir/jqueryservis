70615712fea1c68ca898149903ff63c2086c5fd1
Ahmet Demir <43875085+SoftwareEngAhmetDemir@users.noreply.github.com>
2022-06-14 15:44:03 +0300

    Update btn.js

--- btn.js
+++ btn.js
@@ -1,12 +1,10 @@
-
 let footercerez = document.querySelector('.cerez-footer');
 
 document.querySelector('.footer-btn').onclick = function() {
     'use strict';
     localStorage.setItem('okundu', true);
     footercerez.addClass('d-none');
-});
+}
 if (localStorage.getItem('okundu')) {
     footercerez.addClass('d-none');
 }
-
